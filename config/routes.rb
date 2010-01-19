c43e502d35f5c5dccc92a8235e46f0e997431b5d
Tyler Gannon <tgannon@gmail.com>
2010-01-19 09:26:07 -0800

    Fixing route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :formula_roles
 
   map.resources :formulas
 	map.resources :formula_roles
@@ -48,7 +47,7 @@ ActionController::Routing::Routes.draw do |map|
   #   end
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
+  map.root :controller => "formulas"
 
   # See how all your routes lay out with "rake routes"
 
