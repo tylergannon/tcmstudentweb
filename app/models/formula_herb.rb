4e99f9778b3b666c6e1192ce8e364a099b19bacc
Tyler Gannon <tgannon@gmail.com>
2010-03-30 11:59:41 -0700

    Should be able to match any herb.

--- app/models/formula_herb.rb
+++ app/models/formula_herb.rb
@@ -2,19 +2,22 @@ class FormulaHerb < ActiveRecord::Base
 	belongs_to :formula
 	belongs_to :herb
 	belongs_to :formula_role
-	
+
 	def herb_pinyin=(pinyin)
-		self.herb = Herb.find_or_create_by_pinyin(pinyin) unless pinyin.blank?
+    return if pinyin.blank
+    a = Herb.search(pinyin, :first)
+    raise "Could not find herb #{pinyin}" unless a
+    self.herb = a
 	end
-	
+
 	def herb_pinyin
 		herb.pinyin if herb
 	end
-	
+
 	def formula_role_pinyin=(pinyin)
 		self.formula_role = FormulaRole.find_or_create_by_pinyin(pinyin) unless pinyin.blank?
 	end
-	
+
 	def formula_role_pinyin
 		formula_role.pinyin if formula_role
 	end
