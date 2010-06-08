6ed8fb63773ad53c27251b4ec1250cf9e85bf954
Tyler Gannon <tgannon@gmail.com>
2010-06-07 22:32:22 -0700

    hkjg

--- app/controllers/pattern_symptoms_controller.rb
+++ app/controllers/pattern_symptoms_controller.rb
@@ -1,10 +1,9 @@
 class PatternSymptomsController < ApplicationController
   def sort
-    params[:PatternSymptoms].each_with_index do |id, index|
+    params[:pattern_symptoms].each_with_index do |id, index|
       PatternSymptom.update_all(['position=?', index+1], ['id=?', id])
     end
     render :nothing => true
   end
-
 end
 
