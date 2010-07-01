f4360c2779e027cd82501bec03394fe4ec5831f6
Tyler Gannon <tgannon@gmail.com>
2010-07-01 12:47:46 -0700

    sdfg

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -11,6 +11,16 @@ class Symptom < ActiveRecord::Base
     end
     a
   end
+  
+  def self.consolidate!(id, condemned_id)
+    symptom = find(id)
+    condemned = find(condemned_id)
+    condemned.pattern_symptoms.each do |ps|
+      ps.symptom = symptom
+      ps.save
+    end
+    condemned.destroy
+  end
 
   def =~ (other)
     false if other.nil? || other.class != Symptom
