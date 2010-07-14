bf1b1826bb3624cf244df8e718be44cb86dbbf21
Tyler Gannon <tgannon@gmail.com>
2010-07-13 19:08:08 -0700

    One more try

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -2,12 +2,13 @@ class TagsController < ApplicationController
   respond_to :json, :only => :index
 
   def index
-    @tags = ActsAsTaggableOn::Tag.named_like(params[:search]) \
-      .map{|t|
+    @tags = ActsAsTaggableOn::Tag.named_like(params[:search])
+     json = @tags.map{|t|
         {:value=>t.name}
       }.to_json
-
-    respond_with @tags
+    respond_with @tags do |format|
+      format.json {render :json=> json}
+    end
   end
 end
 
