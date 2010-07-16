2d5b313e3d8fab5d9b1be9a31a2f6608fe25e2c1
Tyler Gannon <tgannon@gmail.com>
2010-07-15 22:43:32 -0700

    fixed pp_acu_point error

--- app/models/point_prescription_acu_point.rb
+++ app/models/point_prescription_acu_point.rb
@@ -1,19 +1,19 @@
 class PointPrescriptionAcuPoint < ActiveRecord::Base
   belongs_to :point_prescription
   belongs_to :acu_point
-  acts_as_linkable
-  
+  acts_as_linkable :name => :acu_point_abbrev
   before_save :default_values
-  
+
   scope :with_acu_point_abbrev, lambda{|abbrev|
     ap = AcuPoint.named(abbrev)
     id = ap.id if ap
     joins(:acu_point).where(:acu_points=>{:id=>id})
   }
-  
+
   def default_values
     self.reason = "Primary" unless reason
   end
-  
+
   named_association :acu_point, :abbrev
 end
+
